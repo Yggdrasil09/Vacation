f62eb6756e0773623c05ada4137b76d4e66eb376
Yggdrasil09 <karthik99th.tnk@gmail.com>
2019-03-01 16:20:19 +0530

    Heading is set

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import './App.css';
+import Heading from './components/heading.js';
+
+class App extends Component {
+  render() {
+    return ( 
+		<div>
+			<Heading/>
+		</div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/heading.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Heading extends Component{
+    render()
+    {
+        return(
+            <div className="heading">
+                <h1>Vacation is Here!</h1>
+            </div>
+        );
+    }
+}
+
+export default Heading;
